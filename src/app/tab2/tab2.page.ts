cf5bbe02f714ef3c2d753f6f5a07bf7b7dda58cb
Kelly Cardoso <kellycristinacardoso.713@gmail.com>
2022-10-03 22:27:52 -0300

    0310

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -5,8 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page{
+  operacao = '';
+  resultado = '';
 
   constructor() {}
+
+  adicionarValor(valor: string) {
+    this.operacao += valor;
+  }
+  limparOperacao(){
+    this.operacao = '';
+  }
+
+  limparMemoria(){
+    this.operacao = '';
+    this.resultado = '';
+  }
+
+  apagarCaracter(){
+    if(this.operacao.length > 0) {
+      this.operacao =this.operacao.substring(0, this.operacao.length - 1);
+    }
+  }
 
 }
