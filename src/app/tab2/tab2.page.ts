bcbe1bf316d8f36727dc75c90f9f688fa7c76136
Kelly Cardoso <kellycristinacardoso.713@gmail.com>
2022-10-17 22:25:20 -0300

    sla

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { evaluate } from 'mathjs';
 
 @Component({
   selector: 'app-tab2',
@@ -8,25 +9,54 @@ import { Component } from '@angular/core';
 export class Tab2Page{
   operacao = '';
   resultado = '';
+  numero = false;
+  caracter = true;
+  caracteres = ['.', '/', '*', '+', '-'];
+  ponto = ['.'];
 
   constructor() {}
 
+  ngOnInit() {}
+
+  calcularOperacao(){
+    this.resultado = evaluate(this.operacao);
+  }
+
   adicionarValor(valor: string) {
-    this.operacao += valor;
+    this.caracter = this.caracteres.includes(valor);
+
+    if (!this.caracter) {
+      this.operacao += valor;
+      this.numero = true;
+    } else if (this.caracter && this.numero) {
+      this.operacao += valor;
+      this.numero = false;
+    }
+
+    if (!this.operacao) {
+    }
   }
   limparOperacao(){
     this.operacao = '';
+    this.resultado = '';
+    this.numero = false;
   }
 
   limparMemoria(){
     this.operacao = '';
     this.resultado = '';
+    this.numero = false;
   }
 
   apagarCaracter(){
     if(this.operacao.length > 0) {
       this.operacao =this.operacao.substring(0, this.operacao.length - 1);
     }
-  }
 
+    const ultimo = this.operacao.substring(this.operacao.length, 1);
+    this.caracter = this.caracteres.includes(ultimo);
+
+    console.log(ultimo);
+  }
+  
 }
